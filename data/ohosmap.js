c4fae57917af644ae891a30fb4cf68a2c161a39b
JohnHeeryTNA <john.heery@nationalarchives.gov.uk>
2024-01-30 12:44:04 +0000

    Initial work on ohos map

---
+++ data/ohosmap.js
@@ -0,0 +1,147 @@
+var places = {
+
+	"type": "FeatureCollection",
+	"features": [{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-3.145946015173,
+					52.659327935960
+				]
+			},
+			"properties": {
+				"title": "Tocyn ar gyfer Cerdded y Terfynau, Y Trallwng, 1874",
+				"description": "A ticket issued to participants in the 'Perambulation of the Boundaries of the Borough of Welshpool', which was held over four successive days from 30 September to 3 October 1874. The ticket bears the name of the then Mayor, D. P. Owen. The frequency of this unusual event is not known, though there are photographs of the Perambulation of 1906.",
+				"itemURL": "https://www.peoplescollection.wales/items/10899",
+				"collection": "People's Collection Wales",
+				"subject": [
+					"Communication and Media",
+					"Fairs",
+					"OTHER: Community and Social"
+				]
+			},
+			"id": 0
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-3.290278, // This is approximate based on a search for Llanafan, Cardiganshire. If you have more precise coordinates, please provide them.
+					52.305000 // Same as above.
+				]
+			},
+			"properties": {
+				"title": "St Afan's Parish Church, Llanafan, Cardiganshire",
+				"description": "St Afan's Parish Church, Llanafan, Cardiganshire",
+				"itemURL": "https://www.peoplescollection.wales/collections/1609926",
+				"collection": "People's Collection Wales",
+				"group": "community"
+			},
+			"id": 1
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-4.085111,
+					52.411540
+				]
+			},
+			"properties": {
+				"title": "Protest Pont Trefechan, 2 Chwefror 1963 (7 o 7)",
+				"description": "Gwilym Tudur talks about the unexpected response to the protest on Trefechan Bridge from both the Welsh and English medium press. He mentions the change in behaviour towards the Welsh Language Society in the press today, and also mention the other organizations that have been essential in order to gain a better status for the Welsh language.",
+				"itemURL": "https://www.peoplescollection.wales/items/44640",
+				"collection": "People's Collection Wales",
+				"group": "community",
+				"subject": [
+					"Bridge",
+					"Language",
+					"Protest",
+					"Trefechan",
+					"Welsh"
+				]
+			},
+			"id": 2
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-3.950000, // Using the provided longitude.
+					52.166667 // Using the provided latitude.
+				]
+			},
+			"properties": {
+				"title": "Hen dy cwrdd y Methodistiad Calfinaidd, Llanddewibrefi",
+				"description": "Abandoned and dilapidated building.",
+				"itemURL": "https://www.peoplescollection.wales/items/14775",
+				"collection": "People's Collection Wales",
+				"group": "community",
+				"subject": "'Nonconformist Denominations'"
+			},
+			"id": 3
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-4.05111, // Using the provided longitude.
+					52.511540 // Using the provided latitude.
+				]
+			},
+			"properties": {
+				"title": "Protest Pont Trefechan, 2 Chwefror 1963 (6 o 7)",
+				"description": "Gwilym Tudur talks about his experience on the day of the Welsh Language Society's protest on Trefechan Bridge.",
+				"itemURL": "https://www.peoplescollection.wales/items/44639",
+				"collection": "People's Collection Wales",
+				"group": "community",
+				"subject": ["Bridge", "Language", "Protest", "Trefechan", "Welsh"]
+			},
+			"id": 4
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-3.337894, // Using the provided longitude.
+					52.271699 // Using the provided latitude.
+				]
+			},
+			"properties": {
+				"title": "Copi o ffotograff portread o Mr Poole, tua 1930au, Llandrindod.",
+				"description": "One of a series of copies of portraits by local photographer Rousham Roberts.",
+				"itemURL": "https://www.peoplescollection.wales/items/36762",
+				"collection": "People's Collection Wales",
+				"group": "community",
+				"subject": ["Photographers", "Portraits", "photography"]
+			},
+			"id": 5
+		},
+		{
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [
+					-3.171660, // Using the provided longitude.
+					52.969215 // Using the provided latitude.
+				]
+			},
+			"properties": {
+				"title": "Llangollen.",
+				"description": "Llangollen. Abaty Glyn y Groes.Cyfeirnod: 2004.5.6",
+				"itemURL": "https://www.peoplescollection.wales/items/41076",
+				"collection": "People's Collection Wales",
+				"group": "community",
+				"subject": ["Llangollen", "Valle Crucis Abbey"]
+			},
+			"id": 6
+		}
+	]
+
+};
